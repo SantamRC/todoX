bbafbf3d7046a247351b5a635f082a01ccc6508c
SantamRC <santamdev404@gmail.com>
2023-05-08 14:40:41 +0530

    Added Models

---
+++ src/Models/task.ts
@@ -0,0 +1,24 @@
+import mongoose, { Document } from "mongoose";
+
+export interface ITodo extends Document {
+  title: string;
+  description: string;
+  status: boolean;
+}
+
+const todoSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  status: {
+    type: Boolean,
+    required: true,
+  },
+});
+
+export default mongoose.model<ITodo>("Todo", todoSchema);
