8dbf50abc50a0857e14efa0880b1d6bf09c9769e
SantamRC <santamdev404@gmail.com>
2023-05-08 15:00:32 +0530

    Changed App Code

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,26 @@
-import express from "express";
-const app = express();
-const port = 3000;
+import express, { Application } from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import bodyParser from "body-parser";
+import todoRoutes from "./Routes/todo";
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+const app: Application = express();
 
-app.listen(port, () => {
-  return console.log(`Express is listening at http://localhost:${port}`);
-});
+const PORT: string | number = process.env.PORT || 4000;
+const DB: string = process.env.MONGODB_URI || "mongodb://localhost:27017/todos";
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use("/api", todoRoutes);
+
+mongoose
+  .connect(DB)
+  .then(() => {
+    console.log(`MongoDB connected`);
+    app.listen(PORT, () =>
+      console.log(`Server running on http://localhost:${PORT}`)
+    );
+  })
+  .catch((error) => {
+    console.log("MongoDB connection error:", error);
+  });
